585ecc263126afec6fec42d7c2974688bd21d2e2
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2012-12-20 16:47:31 -0700

    Initial commit

---
+++ background.js
@@ -0,0 +1,14 @@
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab) {
+	//matches examples
+	//na4.salesforce.com
+	//cs13.salesforce.com
+	//company.my.salesforce.com
+	//emea.salesforce.com
+	if (tab.url.match(/(ap|eu|na|cs|emea|.*\.my)[0-9]*\.salesforce.com/) !== null) {
+		chrome.pageAction.show(tabId);
+	}
+}
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
